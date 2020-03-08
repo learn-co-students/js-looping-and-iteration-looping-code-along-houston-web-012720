c331d6b506534a4ad78cebadf64547e3994a05d7
vienpham2019 <vienpham2019@gmail.com>
2020-03-08 11:26:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const writeCards = (array,name) => {
+    let cardArray = [];
+    for(let i = 0; i < array.length; i++){
+        cardArray.push(`Thank you, ${array[i]}, for the wonderful ${name} gift!`)
+    }
+    return cardArray
+}
+
+const countDown = (number) => {
+    let i = number 
+    while (i >= 0){
+        console.log(i)
+        i --
+    }
+}
