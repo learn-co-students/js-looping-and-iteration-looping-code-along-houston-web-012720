5ec7a83ac4c7a2f1ddcea45245ca21c0f6d47d3d
Gavrilajava <georgy.gavrilchik@gmail.com>
2020-03-07 21:13:54 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(arr) {
+  let i = 0
+  while(i < arr.length){
+    arr [i] = (`Thank you, ${arr[i]}, for the wonderful surprise gift!`)
+    i++
+  }
+  return arr
+}
+
+function countDown(number) {
+  for(let i = number; i >= 0; i--){
+    console.log(i)
+  }
+}
