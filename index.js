910345749535eabbb6ddd75f766df3437c84e98c
placidowang <placidow@gmail.com>
2020-03-08 20:44:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+  let messages = [];
+  for (name of names) {
+    messages.push(`Thank you, ${name}, for the wonderful ${event} gift!`);
+  }
+  return messages;
+}
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
