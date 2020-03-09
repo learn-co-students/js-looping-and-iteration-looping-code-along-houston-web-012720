4409eb28710394d54fe3ac9893c3abbb48eb697c
timothyalton <mitchell.alton@live.com>
2020-03-09 09:15:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+// array = ["Lisa", "Kaitlin", "Jan"]
+// event = 'surprise'
+function writeCards(array, event) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArray
+}
+
+
+function countDown(num) {
+    let i = 0;
+    while (i <= num) {
+        console.log(i++)
+    }
+
+}
